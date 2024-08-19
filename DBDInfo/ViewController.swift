c3b9e4598f3e9601d8bb40e34d5717b4f1a890c8
Mauzec <mauzc@vivaldi.net>
2024-08-19 15:06:29 +0300

    Initial Commit

---
+++ DBDInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DBDInfo
+//
+//  Created by Timur Murzakov on 8/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
